3cfde858edc037159ddb0b145a2ebe9d0ec172cb
caoxubo <caoxb2004@163.com>
2026-07-29 16:12:20 +0100

    optimization methods

--- grid_search.py
+++ grid_search.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import t
 
+from experiment import turn_on_threshold
 from simulator import server_simulator
 from src.Config import *
 
@@ -27,3 +28,60 @@ GRID_SEARCH_SEEDS = list(range(100, 110))
 # Result directory
 RESULT_DIR = "experiment_results/grid_search/"
 os.makedirs(RESULT_DIR, exist_ok=True)
+
+def run_grid_search(turn_off_threshold_values, turn_on_threshold_values, seeds):
+
+    all_records = []
+
+    best_objective = np.inf
+    best_turn_off_threshold = None
+    best_turn_on_threshold = None
+
+    evaluation_id = 0
+
+    for turn_off_threshold in turn_off_threshold_values:
+        for turn_on_threshold in turn_on_threshold_values:
+
+            # Check if the threshold pair is feasible
+            if not threshold_constraints(turn_off_threshold, turn_on_threshold):
+                continue
+
+            evaluation_id += 1
+
+            print(f"Start grid search: Evaluation ID: {evaluation_id}")
+            print(f"Threshold pair (T_i, T_o): ({turn_on_threshold, turn_off_threshold})")
+
+            # Start evaluate the threshold pairs
+            mean_objective, records = estimate_objective(turn_off_threshold=turn_off_threshold,
+                                                         turn_on_threshold=turn_on_threshold,
+                                                         seeds=seeds,
+                                                         evaluation_id=evaluation_id,
+                                                         unrounded_turn_on_threshold=turn_off_threshold,
+                                                         unrounded_turn_off_threshold=turn_on_threshold,
+                                                         policy=POLICY,
+                                                         objective_type=OBJECTIVE_TYPE
+                                                         )
+
+            if not np.isfinite(mean_objective):
+                mean_objective = LARGE_PENALTY
+
+            # Update the best objective and its threshold pair
+            if mean_objective < best_objective:
+                best_objective = mean_objective
+                best_turn_off_threshold = turn_off_threshold
+                best_turn_on_threshold = turn_on_threshold
+
+            # Recording the experimental results
+            for record in records:
+                record["phase"] = "grid_search"
+                record["mean_objective_this_evaluation"] = mean_objective
+                record["best_objective"] = best_objective
+                record["best_T_i"] = best_turn_off_threshold
+                record["best_T_o"] = best_turn_on_threshold
+                record["num_seeds"] = len(seeds)
+
+            all_records.extend(records)
+
+            print(f"Finished grid search: Evaluation ID: {evaluation_id}")
+            print(f"mean objective={mean_objective:.6f}")
+
